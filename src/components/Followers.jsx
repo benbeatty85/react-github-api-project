d6e5b251258ae1cebc0a9bed9adf5f18db991c61
Ben Beatty <benbeatty85@gmail.com>
2017-08-05 15:04:54 +0000

    finished infinite scrolling for followers page

--- src/components/Followers.jsx
+++ src/components/Followers.jsx
@@ -1,61 +1,62 @@
 import React from 'react';
-import { Link } from 'react-router';
+//import { Link } from 'react-router';
 import GithubUser from './GithubUser';
+import Infinite from 'react-infinite';
+
 class Followers extends React.Component {
-    
-    constructor() {
-        super();
-        this.state = {};
-    }
-    
-    fetchData() {
-        //console.log(this)
-        fetch(`https://api.github.com/users/${this.props.params.username}/followers`)
-        .then(response => response.json())
-        .then(
-            followers => {
-                this.setState({
-                    followers: followers
-                });
-            }
-        )
-        // .then(
-        //     console.log(this)
-        // )
-    }
-    
-     componentDidMount() {
-        this.fetchData();
-    }
-    
-    componentDidUpdate(prevProps, prevState) {
-        if (prevProps.params.username !== this.props.params.username) { 
-          this.fetchData();
-      }
-    }
-    
-    render() {
-        if (!this.state.followers) {
-            return <div>LOADING FOLLOWERS...</div>;
+ constructor() {
+    super();
+    this.state = {
+      page: 1,
+      loading: false,
+      followers: []
+    };
+}
+ fetchData = () => {
+   this.setState({
+      loading: true
+    });
+    //console.log(this)
+    fetch(`https://api.github.com/users/${this.props.params.username}/followers?&page=${this.state.page}&per_page=50`)
+      .then(response => response.json())
+      .then(
+        followersResponse => {
+          console.log('got response');
+         let mergedArray = this.state.followers.concat(followersResponse);
+          this.setState({
+            followers: mergedArray,
+            page: this.state.page + 1,
+            loading: false
+          });
         }
-        //console.log(this.state.followers.object.username);
-        return (
-            <div className="followers-page">
-                <h3>Followers of {this.props.params.username}</h3>
-                <ul>
-                    {this.state.followers.map(follower => { 
-                    return(
-                        <li key={follower.id}>
-                            <GithubUser user={follower.login} avatar_url={follower.avatar_url} img src={follower.avatar_url} />  
-                            
-                        </li>
-                        )
-                    })}
-                      
-                  
-                </ul>    
-            </div>
-        );
+      );
+    
+  }
+  
+ componentDidUpdate(prevProps, prevState) {
+    if (prevProps.params.username !== this.props.params.username) {
+      this.fetchData();
     }
+  }
+ render() {
+    console.log('in render', this.state.followers);
+    
+    return (
+      <div className="followers-page">
+        <h3>Followed by {this.props.params.username}</h3>
+        <Infinite className="followers-page"
+                  isInfiniteLoading={this.state.loading}
+                  onInfiniteLoad={this.fetchData.bind(this)}
+                  useWindowAsScrollContainer
+                  elementHeight={100}
+                  infiniteLoadBeginEdgeOffset={100} >
+        {this.state.followers.map( follower => {
+            return <GithubUser user={follower.login} key={follower.id} avatar_url={follower.avatar_url} />
+        }
+        )}
+        </Infinite>
+      </div>
+    );
+  }
 }
 export default Followers;
--- src/components/GithubUser.jsx
+++ src/components/GithubUser.jsx
@@ -4,13 +4,16 @@ import { Link } from 'react-router';
 class GithubUser extends React.Component {
     
     render() {
+        // console.log(this.props)
         return (
-            <div>
-                <Link className="githubUserLink" to={`/user/${this.props.user}`}>
-                    <h2 className="githubUser-name">{this.props.user}</h2>
+        <div>
+            <Link className="githubUserLink" to={`/user/${this.props.user}`}>
+                <div className="githubUserDisplay">
                     <img className="githubUser-avatar" src={this.props.avatar_url} alt={`${this.props.user} avatar`} />
-                </Link>
-            </div>
+                    <h2 className="githubUser-name">{this.props.user}</h2>
+                </div>    
+            </Link>
+        </div>
         );
     }
 }
