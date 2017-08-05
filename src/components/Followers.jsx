352a05e76cb8dec8bfb74cbf37f7c06fc7f51cee
Ben Beatty <benbeatty85@gmail.com>
2017-08-05 15:21:30 +0000

    changes to followers page

--- src/components/Followers.jsx
+++ src/components/Followers.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-//import { Link } from 'react-router';
+// import { Link } from 'react-router';
 import GithubUser from './GithubUser';
 import Infinite from 'react-infinite';
 
 class Followers extends React.Component {
- constructor() {
+
+constructor() {
     super();
     this.state = {
       page: 1,
@@ -12,8 +13,10 @@ class Followers extends React.Component {
       followers: []
     };
 }
- fetchData = () => {
-   this.setState({
+
+fetchData = () => {
+
+  this.setState({
       loading: true
     });
     //console.log(this)
@@ -21,8 +24,9 @@ class Followers extends React.Component {
       .then(response => response.json())
       .then(
         followersResponse => {
-          console.log('got response');
-         let mergedArray = this.state.followers.concat(followersResponse);
+          //console.log('got response');
+
+        let mergedArray = this.state.followers.concat(followersResponse);
           this.setState({
             followers: mergedArray,
             page: this.state.page + 1,
@@ -30,31 +34,44 @@ class Followers extends React.Component {
           });
         }
       );
-    
+    // .then(
+    //     console.log(this)
+    // )
   }
-  
- componentDidUpdate(prevProps, prevState) {
+  //
+  // componentDidMount() {
+  //   this.fetchData();
+  // }
+
+componentDidUpdate(prevProps, prevState) {
     if (prevProps.params.username !== this.props.params.username) {
       this.fetchData();
     }
   }
- render() {
-    console.log('in render', this.state.followers);
-    
+
+render() {
+    //console.log('in render', this.state.followers);
+    //
+    // if (!this.state.followers) {
+    //   return <div>LOADING followers...</div>;
+    // }
     return (
       <div className="followers-page">
-        <h3>Followed by {this.props.params.username}</h3>
-        <Infinite className="followers-page"
-                  isInfiniteLoading={this.state.loading}
-                  onInfiniteLoad={this.fetchData.bind(this)}
-                  useWindowAsScrollContainer
-                  elementHeight={100}
-                  infiniteLoadBeginEdgeOffset={100} >
-        {this.state.followers.map( follower => {
-            return <GithubUser user={follower.login} key={follower.id} avatar_url={follower.avatar_url} />
-        }
-        )}
-        </Infinite>
+        <h3>Followers of {this.props.params.username}</h3>
+        <div className="Infinite-scroll-followers">
+            <Infinite className="followers-page-infinite"
+                      isInfiniteLoading={this.state.loading}
+                      onInfiniteLoad={this.fetchData.bind(this)}
+                      useWindowAsScrollContainer
+                      elementHeight={100}
+                      infiniteLoadBeginEdgeOffset={100} >
+                {this.state.followers.map(follower => {
+                      return <GithubUser user={follower.login} key={follower.id} avatar_url={follower.avatar_url} />
+                    
+               }
+                )}
+            </Infinite>
+        </div>
       </div>
     );
   }
